80d361f61eca6f2bd30913f12fa22c52bce6e5f0
giulio <giuliop@gmail.com>
2025-09-03 23:19:23 +0200

    Organize go imports on save

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -19,14 +19,25 @@ autocmd("FileType", {
   end,
 })
 
--- Format Go files on save with gofmt
+-- Format and organize imports for Go files on save
 autocmd("BufWritePre", {
   group = augroup("GoFormat", { clear = true }),
   pattern = "*.go",
   callback = function()
+    local params = vim.lsp.util.make_range_params(nil, nil, 0, "utf-16")
+    params.context = { only = { "source.organizeImports" } }
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 1000)
+    for cid, res in pairs(result or {}) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+          vim.lsp.util.apply_workspace_edit(r.edit, enc)
+        end
+      end
+    end
     vim.lsp.buf.format({ async = false })
   end,
-  desc = "Format Go files on save",
+  desc = "Organize imports and format Go files on save",
 })
 
 -- Remember last cursor position
